b6aeea84754fbbff1f2cce1b550a1d63821b61bc
Thiago Fernandes <thiagofernandes101@outlook.com>
2025-02-13 22:35:08 -0300

    Add StockQuantity mapping in AutoMapper profile
    Implemented a mapping for the `StockQuantity` property in the `DtoToDomainEntityMappingProfile` class

--- src/NerdStore.Catalog.Application/AutoMapper/DtoToDomainEntityMappingProfile.cs
+++ src/NerdStore.Catalog.Application/AutoMapper/DtoToDomainEntityMappingProfile.cs
@@ -18,6 +18,7 @@ namespace NerdStore.Catalog.Application.AutoMapper
                 .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId.Value))
                 .ForMember(dest => dest.RegisterDate, opt => opt.MapFrom(src => RegisterDate.NewRegisterDate(src.RegisterDate.Value)))
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => ImageHash.NewImageHash(src.Image.Value)))
+                .ForMember(dest => dest.StockQuantity, opt => opt.MapFrom(src => StockQuantity.NewStock(src.StockQuantity.Value)))
                 .ForMember(dest => dest.Dimension, opt => opt.MapFrom(src => Dimension.NewDimension(src.Height.Value, src.Width.Value, src.Depth.Value)));
         }
     }

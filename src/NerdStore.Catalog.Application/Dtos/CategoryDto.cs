f63d1b434894482bf4de55da61c24078f12739a6
Thiago Fernandes <thiagofernandes101@outlook.com>
2025-02-15 21:37:10 -0300

    Refactor CategoryDto to class with validation attributes
    The `CategoryDto` has been changed from a record to a class. A constructor has been added to initialize the properties `Id`, `Name`, and `Cod`. The property `Code` has been renamed to `Cod`, and all properties now use `get; init;` accessors. Data annotations `[Key]` and `[Required]` have been added for validation.

--- src/NerdStore.Catalog.Application/Dtos/CategoryDto.cs
+++ src/NerdStore.Catalog.Application/Dtos/CategoryDto.cs
@@ -1,10 +1,26 @@
-﻿namespace NerdStore.Catalog.Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NerdStore.Catalog.Application.Dtos
 {
-    public record CategoryDto(
-        CategoryDtoId Id,
-        CategoryDtoName Name,
-        CategoryDtoCode Code
-    );
+    public record CategoryDto
+    {
+        public CategoryDto(CategoryDtoId id, CategoryDtoName name, CategoryDtoCode code)
+        {
+            Id = id;
+            Name = name;
+            Cod = code;
+        }
+
+        [Key]
+        public CategoryDtoId Id { get; init; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        public CategoryDtoName Name { get; init; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        public CategoryDtoCode Cod { get; init; }
+    }
+
 
     public record CategoryDtoId(Guid Value);
     public record CategoryDtoName(string Value);
